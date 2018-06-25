0b433ece2a287d9617dd369668c34878939b141d
YuAo <me@imyuao.com>
2018-06-25 18:00:06 +0800

    demo update

--- Example/MetalPetalJS/script.js
+++ Example/MetalPetalJS/script.js
@@ -6,6 +6,19 @@
  
  var image = MTIImage.imageWithContentsOfFileOptionsAlphaType(MTIJSEnvironment.pathByAppendingPathComponentToPath("test.jpg", MTIJSEnvironment.mainBundlePath()), {MTKTextureLoaderOptionSRGB: false}, MTIAlphaType.alphaIsOne);
  
+ /*
+  var grayScaleFilter = MTINativeFilter.filterWithName("MTISaturationFilter");
+  grayScaleFilter.setValueForKey(0.0, "saturation");
+  grayScaleFilter.setValueForKey(image, "inputImage");
+  image = grayScaleFilter.valueForKey("outputImage");
+  
+  var filter = MTINativeFilter.filterWithName("MTIMPSGaussianBlurFilter");
+  filter.setValueForKey(20.0, "radius");
+  filter.setValueForKey(image, "inputImage");
+  
+  image = filter.valueForKey("outputImage");
+  */
+ 
  var geometry = MTIVertices.squareVerticesForRect({ x: -1, y: -1, width: 2, height: 2});
  var command = MTIRenderCommand.renderCommandWithKernelGeometryImagesParameters(kernel, geometry, [image], {});
  
