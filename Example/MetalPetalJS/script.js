41db28de446ec2ecb55439f968eaf24d2c98b492
YuAo <me@imyuao.com>
2018-06-27 14:01:58 +0800

    demo update

--- Example/MetalPetalJS/script.js
+++ Example/MetalPetalJS/script.js
@@ -1,32 +1,41 @@
-(function() {
- console.log(MTIImage);
- 
- var kernel = MTIRenderPipelineKernel.kernelWithJSONDescriptor({ fragmentFunction: { name: "shader", library: MTIJSUtilities.joinPath(MTIJSEnvironment.mainBundlePath,"default.metallib")},
-                                                               vertexFunction: { name: "passthroughVertex" }});
- 
- var image = MTIImage.imageWithContentsOfFileOptionsAlphaType(MTIJSUtilities.joinPath(MTIJSEnvironment.mainBundlePath, "test.jpg"), {MTKTextureLoaderOptionSRGB: false}, MTIAlphaType.alphaIsOne);
- 
- /*
-  var grayScaleFilter = MTINativeFilter.filterWithName("MTISaturationFilter");
-  grayScaleFilter.setValueForKey(0.0, "saturation");
-  grayScaleFilter.setValueForKey(image, "inputImage");
-  image = grayScaleFilter.valueForKey("outputImage");
-  
-  var filter = MTINativeFilter.filterWithName("MTIMPSGaussianBlurFilter");
-  filter.setValueForKey(20.0, "radius");
-  filter.setValueForKey(image, "inputImage");
-  
-  image = filter.valueForKey("outputImage");
-  */
- 
- var geometry = MTIVertices.squareVerticesForRect({ x: -1, y: -1, width: 2, height: 2});
- var command = MTIRenderCommand.renderCommandWithKernelGeometryImagesParameters(kernel, geometry, [image], {});
- 
- var descriptor = MTIRenderPassOutputDescriptor.renderPassOutputDescriptorWithSizePixelFormat({width: image.size.width, height: image.size.height}, 0);
- 
- var outputImage = MTIRenderCommand.imagesByPerformingRenderCommandsOutputDescriptors([command], [descriptor])[0];
- 
- console.log(outputImage);
- 
- return outputImage;
+(function () {
+    console.log(MTIImage);
+
+    var kernel = MTIRenderPipelineKernel.kernelWithJSONDescriptor({
+        fragmentFunction: { 
+            name: "shader", 
+            library: MTIJSUtilities.joinPath(MTIJSEnvironment.mainBundlePath, "default.metallib") 
+        },
+        vertexFunction: {
+             name: "passthroughVertex" 
+        }
+    });
+
+    var image = MTIImage.imageWithContentsOfFileOptionsAlphaType(
+        MTIJSUtilities.joinPath(MTIJSEnvironment.mainBundlePath, "test.jpg"),
+        { MTKTextureLoaderOptionSRGB: false },
+        MTIAlphaType.alphaIsOne);
+    
+    /*
+     var grayScaleFilter = MTINativeFilter.filterWithName("MTISaturationFilter");
+     grayScaleFilter.setValueForKey(0.0, "saturation");
+     grayScaleFilter.setValueForKey(image, "inputImage");
+     image = grayScaleFilter.valueForKey("outputImage");
+     
+     var filter = MTINativeFilter.filterWithName("MTIMPSGaussianBlurFilter");
+     filter.setValueForKey(20.0, "radius");
+     filter.setValueForKey(image, "inputImage");
+     
+     image = filter.valueForKey("outputImage");
+     */
+
+    var geometry = MTIVertices.squareVerticesForRect({ x: -1, y: -1, width: 2, height: 2 });
+
+    var command = MTIRenderCommand.renderCommandWithKernelGeometryImagesParameters(kernel, geometry, [image], {});
+
+    var descriptor = MTIRenderPassOutputDescriptor.renderPassOutputDescriptorWithSizePixelFormat(image.size, 0);
+
+    var outputImage = MTIRenderCommand.imagesByPerformingRenderCommandsOutputDescriptors([command], [descriptor])[0];
+
+    return outputImage;
 })();
